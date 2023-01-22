79b3168db8fb4412c9ea15d4145c58acf66620b7
Luiz Gustavo Machado <luiz.gustavo.machado@hotmail.com>
2023-01-22 20:16:24 -0300

    end proximo nível

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
-const button = document.querySelector('header button')
+const button = document.querySelector("header button")
 
-button.addEventListener('click', add)
+button.addEventListener("click", add)
+form.addEventListener("change", save)
 
 function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
-
   const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
-    alert('Dia já incluso, tente outra data!')
+    alert("Dia já incluso, tente outra data!")
     return
   }
 
-  alert('Adicionado com sucesso')
+  alert("Adicionado com sucesso ✅")
   nlwSetup.addDay(today)
 }
 
-function save(){
-  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
-const data = JSON.parse(localStorage.getItem('NLWSetup@habits'))
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
 
